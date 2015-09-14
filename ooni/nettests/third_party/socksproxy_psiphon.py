e85c5a5e9f5b9d3024e9145ac671ecc630809854
juga0 <juga>
2015-09-14 15:39:16 +0000

    add psiphon test based on the socksproxy test

---
+++ ooni/nettests/third_party/socksproxy_psiphon.py
@@ -0,0 +1,113 @@
+from twisted.internet import defer, reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint
+from twisted.web.client import readBody
+from twisted.python import usage
+
+from txsocksx.http import SOCKS5Agent
+
+from ooni.errors import handleAllFailures, TaskTimedOut
+from ooni.utils import log
+from ooni.templates import process
+from ooni.templates.process import ProcessTest# , ProcessDirector
+
+
+
+class UsageOptions(usage.Options):
+    log.debug("UsageOptions")
+    optParameters = [
+        ['url', 'u', None, 'Specify a single URL to test.'],
+        ['psiphonpath', 'p', None, 'Specify psiphon python client path.'],]
+
+class SocksProxyTest(process.ProcessTest):
+    
+    """
+    This class tests Psiphon python client
+    
+    test_psiphon:
+      Starts a Psiphon, check if it bootstraps successfully
+      (print a line in stderr).
+      Then, perform an HTTP request using the proxy
+    """
+
+    name = "Psiphon Test"
+    description = "Bootstraps a Psiphon and does a HTTP GET for the specified URL"
+    author = "juga"
+    version = "0.0.1"
+    timeout = 20
+    usageOptions = UsageOptions
+    #requiredOptions = ['url']
+    
+    def setUp(self):
+        log.debug('SocksProxyTest: setUp')
+        if self.localOptions['url']:
+            self.url = self.localOptions['url']
+        else:
+            self.url = 'https://wtfismyip.com/text'
+            # FIXME: use http://google.com?
+        if self.localOptions['psiphonpath']:
+            self.psiphonpath = self.localOptions['psiphonpath']
+        else:
+            # FIXME: search for pyclient path instead of assuming is in the home
+            import os
+            self.psiphonpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                '../../../../psiphon-circumvention-system/pyclient')
+            log.debug('psiphon path: %s' % self.psiphonpath)
+
+        x = """#!/usr/bin/env python
+import sys
+sys.path.insert(0, '%s')
+sys.path.insert(0, '%s/SocksiPy')
+from psi_client import connect
+connect(False)
+""" % (self.psiphonpath,  self.psiphonpath)
+
+        import tempfile
+        import stat
+        import os
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.write(x)
+        f.close()
+        os.chmod(f.name, os.stat(f.name).st_mode | stat.S_IEXEC)
+        log.debug('executable file name: %s' % f.name)
+        self.command = [f.name]
+        log.debug('command: %s' % ''.join(self.command))
+        
+    @defer.inlineCallbacks
+    def test_psiphon(self):
+        log.debug('SocksProxyTest: test_psiphon')
+
+        bootstrapped = defer.Deferred()
+        def checkBootstrapped(pd):
+            log.debug("SocksProxyTest: test_psiphon: checkBootstrapped")
+            #if 'Your Psiphon is now running at ' in pd.stderr:
+            if 'Press Ctrl-C to terminate.' in pd.stdout or \
+                'Press Ctrl-C to terminate.' in pd.stderr:
+                if not bootstrapped.called:
+                    bootstrapped.callback(None)
+        finished = self.run(self.command,  readHook=checkBootstrapped)
+        serverEndpoint = TCP4ClientEndpoint(reactor, '127.0.0.1', 1080)
+        agent = SOCKS5Agent(reactor, proxyEndpoint=serverEndpoint)
+        
+        def addResultToReport(result):
+            log.debug("SocksProxyTest: test_psiphon: addResultToReport")
+            self.report['body'] = result
+            self.report['success'] = True
+
+        def addFailureToReport(failure):
+            log.debug("SocksProxyTest: test_psiphon: addFailureToReport")
+            self.report['failure'] = handleAllFailures(failure)
+            self.report['success'] = False
+
+        def doRequest(noreason):
+            log.debug("SocksProxyTest: test_psiphon: doRequest")
+            log.debug("Doing HTTP request via sockx proxy for %s" % self.url)
+            request = agent.request("GET", self.url)
+            request.addCallback(readBody)
+            request.addCallback(addResultToReport)
+            request.addCallback(self.processDirector.close)
+            #request.addCallback(stop)
+            return request
+
+        bootstrapped.addCallback(doRequest)
+        bootstrapped.addErrback(addFailureToReport)
+        yield bootstrapped
